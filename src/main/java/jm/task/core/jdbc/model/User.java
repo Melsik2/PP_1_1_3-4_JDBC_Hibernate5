c572d4b5692a01c1bdabbc54f0f9bed9b75f69b6
melsikdavtan <melsik.davtyan@bk.ru>
2025-06-03 14:40:11 +0300

    Melsik123

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -20,7 +20,10 @@ public class User {
     private Byte age;
 
     public User(long id, String name, String lastName, byte age) {
-
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
     }
 
     public User(String name, String lastName, Byte age) {
@@ -57,6 +60,10 @@ public class User {
         return age;
     }
 
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -71,15 +78,14 @@ public class User {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(age, user.age);
+        return Objects.equals(id, user.id) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(age, user.age);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id, name, lastName, age);
-    }
-
-    public void setAge(Byte age) {
-        this.age = age;
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Util {
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/schema2?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/schema1";
     private static final String USER = "root";
     private static final String PASSWORD = "melsik2002";
     public static Connection getConnection() {
