492a4e94a103554ac7c09dc4012ca5816d409780
melsikdavtan <melsik.davtyan@bk.ru>
2025-06-09 17:25:53 +0300

    Melsik123

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,94 +3,126 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import java.util.Collections;
+
 import java.util.List;
 
-public class UserDaoHibernateImpl implements UserDao {
+
+public class UserDaoHibernateImpl extends Util  implements UserDao {
+    private SessionFactory sessionFactory;
+    public UserDaoHibernateImpl() {
+
+    }
 
     @Override
     public void createUsersTable() {
-        Transaction tx = null;
-        String sql = "CREATE TABLE IF NOT EXISTS users (" +
-                "id INT PRIMARY KEY AUTO_INCREMENT, " +
-                "name VARCHAR(255), " +
-                "lastName VARCHAR(255), " +
-                "age TINYINT)";
+        Transaction transaction = null;
+
+
+        String sql = "CREATE TABLE IF NOT EXISTS users (ID INT AUTO_INCREMENT PRIMARY KEY," +
+                " NAME VARCHAR(255), LASTNAME VARCHAR(255), AGE INT)";
+
         try (Session session = Util.getSessionFactory().openSession()) {
-            tx = session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createSQLQuery(sql).executeUpdate();
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public void dropUsersTable() {
-        Transaction tx = null;
+        Transaction transaction = null;
+        String sql = "DROP TABLE IF EXISTS users";
         try (Session session = Util.getSessionFactory().openSession()) {
-            tx = session.beginTransaction();
-            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+
+            }
         }
     }
 
+
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Transaction tx = null;
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            tx = session.beginTransaction();
-            session.save(new User(name, lastName, age));
-            tx.commit();
-            System.out.println("Пользователь с именем – " + name + " добавлен в базу данных");
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
+            transaction = session.beginTransaction();
+            User user = new User();
+            user.setName(name);
+            user.setLastName(lastName);
+            user.setAge(age);
+            session.save(user);
+            transaction.commit();
+            System.out.println("User с именем " + name + " добавлен в базу данных через хибернейт"); //?????????
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
         }
+
     }
 
     @Override
     public void removeUserById(long id) {
-        Transaction tx = null;
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            tx = session.beginTransaction();
+            transaction = session.beginTransaction();
             User user = session.get(User.class, id);
             if (user != null) {
                 session.delete(user);
+            }else {
+                System.out.println("пользователь не найден");
             }
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
+
+
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             e.printStackTrace();
         }
+
     }
 
     @Override
     public List<User> getAllUsers() {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            return session.createQuery("FROM User", User.class).list();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Collections.emptyList();
+            transaction = session.beginTransaction();
+            List<User> users = session.createQuery("from User").list();
+            transaction.commit();
+            return users;
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
+        return null;
     }
 
     @Override
     public void cleanUsersTable() {
-        Transaction tx = null;
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            tx = session.beginTransaction();
-            session.createSQLQuery("TRUNCATE TABLE users").executeUpdate();
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DELETE FROM users").executeUpdate(); //и здесь намучался, ошибка была в том что в классе юзер не
+            transaction.commit();                                           //дал название в этой штуке @Table(name = "users")
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 }
